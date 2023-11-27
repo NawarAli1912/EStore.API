5c6da8f84aa7de89a39ae58f5321e1a13d2ebdb1
Nawar Ali <nr.ali1912@gmail.com>
2023-11-28 00:43:05 +0300

    setup-global-error-handling

---
+++ Presentation/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Presentation.Common.Errors;
+
+namespace Presentation;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddSingleton<ProblemDetailsFactory, EStoreProblemDetailsFactory>();
+
+        return services;
+    }
+}
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,33 +1,28 @@
 using Infrastructure;
+using Presentation;
 
 var builder = WebApplication.CreateBuilder(args);
 {
+    builder.Services
+        .AddPresentation()
+        .AddInfrastructure(builder.Configuration);
 
-    // Add services to the container.
-
-    builder.Services.AddControllers();
-    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
     builder.Services.AddEndpointsApiExplorer();
     builder.Services.AddSwaggerGen();
-
-    builder.Services.AddInfrastructure(builder.Configuration);
 
 }
 
 var app = builder.Build();
 {
-    // Configure the HTTP request pipeline.
     if (app.Environment.IsDevelopment())
     {
         app.UseSwagger();
         app.UseSwaggerUI();
     }
 
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
-
     app.UseAuthorization();
-
     app.MapControllers();
-
     app.Run();
 }
